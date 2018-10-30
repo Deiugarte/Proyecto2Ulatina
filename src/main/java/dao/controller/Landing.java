80401eeec336ee15d0f5374b6ec71b1cd2e8d876
Kenneth08 <37820865+Kenneth08@users.noreply.github.com>
2018-10-29 19:43:11 -0600

    El resto de lo que hicimos en clase

--- src/main/java/dao/controller/Landing.java
+++ src/main/java/dao/controller/Landing.java
@@ -1,29 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao.controller;
 
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
- 
+
 @ManagedBean
 public class Landing {
-     
+
     private List<String> images;
-     
+
     @PostConstruct
     public void init() {
         images = new ArrayList<String>();
-        for (int i = 1; i <= 4; i++) {
+        for (int i = 1; i <= 6; i++) {
             images.add("nature" + i + ".jpg");
         }
         System.out.println(images);
     }
- 
+
     public List<String> getImages() {
         return images;
     }
